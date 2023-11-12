b8956d2cd2161a0ced51c8ab53d042b7c3c59d0d
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2023-11-13 02:05:10 +0700

    dev: minor changes

--- src/api/routes/adminRoutes.ts
+++ src/api/routes/adminRoutes.ts
@@ -1,10 +1,19 @@
-import getPendingUsersController from "@/controllers/admin/getPendingUsersController";
-import { Router } from "express";
-import authenticateToken from "../middlewares/authenticateToken";
-import postPendingActionController from "@/controllers/admin/postPendingActionController";
-import { validatePendingAction } from "../middlewares/validate";
+import getPendingUsersController from '@/controllers/admin/getPendingUsersController';
+import { Router } from 'express';
+import authenticateToken from '../middlewares/authenticateToken';
+import postPendingActionController from '@/controllers/admin/postPendingActionController';
+import { validatePendingAction } from '../middlewares/validate';
 
 export default function (app: Router) {
-    app.get("/admin/pending", authenticateToken(true, 'admin'), getPendingUsersController);
-    app.post("/admin/pending/:user_id", authenticateToken(true, 'admin'), validatePendingAction, postPendingActionController);
+    app.get(
+        '/admin/pending',
+        authenticateToken(true, 'admin'),
+        getPendingUsersController
+    );
+    app.post(
+        '/admin/pending/:user_id',
+        authenticateToken(true, 'admin'),
+        validatePendingAction,
+        postPendingActionController
+    );
 }
--- src/api/routes/authRoutes.ts
+++ src/api/routes/authRoutes.ts
@@ -1,9 +1,9 @@
-import login from "@/controllers/auth/login";
-import { Application, Router } from "express";
-import authenticateToken from "../middlewares/authenticateToken";
-import whoami from "@/controllers/auth/whoami";
+import login from '@/controllers/auth/login';
+import { Application, Router } from 'express';
+import authenticateToken from '../middlewares/authenticateToken';
+import whoami from '@/controllers/auth/whoami';
 
 export default function (app: Router) {
-    app.post("/login", login);
-    app.get("/whoami", authenticateToken(null, 'admin', 'user'), whoami);
+    app.post('/login', login);
+    app.get('/whoami', authenticateToken(null, 'admin', 'user'), whoami);
 }
