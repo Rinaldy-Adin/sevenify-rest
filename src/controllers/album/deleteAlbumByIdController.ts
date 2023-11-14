c471e64709b9a6636d131bd58a5999df60c21f41
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2023-11-14 19:41:29 +0700

    feat: add delete album

--- src/controllers/album/deleteAlbumByIdController.ts
+++ src/controllers/album/deleteAlbumByIdController.ts
@@ -3,19 +3,35 @@ import httpResponse from '@/utils/httpResponse';
 import { NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { Request, Response } from 'express';
-import { deleteMusic } from '@/services/musicService';
+import { deleteAlbum } from '@/services/albumService';
 import { logger } from '@/utils/logger';
-import { IMusicResponseDTO } from '@/common/interfaces/IMusic';
+import { IAlbumResponseDTO } from '@/common/interfaces/IAlbum';
 
 export default async function (
-    req: Request<{ music_id: string }, {}, {}, { premium: 'true' | 'false' }>,
+    req: Request<{ album_id: string }, {}, {}, { premium: 'true' | 'false' }>,
     res: Response,
     next: NextFunction
 ) {
     if (!req.user) throw new AppError(StatusCodes.UNAUTHORIZED);
+    logger.info('RUN');
 
     try {
-        return new httpResponse(res, null).json();
+        const album = await deleteAlbum(
+            req.user?.phpSessId,
+            req.user?.id,
+            parseInt(req.params.album_id),
+            req.query.premium == 'true'
+        );
+
+        if (!album) throw new AppError(StatusCodes.NOT_FOUND);
+        const albumDTO: IAlbumResponseDTO = {
+            id: album.id,
+            title: album.name,
+            owner_id: album.ownerId,
+            is_premium: album.isPremium,
+            music_id: album.musicIds,
+        };
+        return new httpResponse(res, {album: albumDTO}, 200, 'Successfully deleted album').json();
     } catch (error) {
         if (error instanceof AppError) {
             next(error);
