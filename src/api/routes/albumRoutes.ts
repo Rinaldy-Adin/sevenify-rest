56de2ff2c9c3490220d0280bc3acf6380b8b184f
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2023-11-14 19:42:12 +0700

    refactor: fix album route

--- src/api/routes/albumRoutes.ts
+++ src/api/routes/albumRoutes.ts
@@ -1,7 +1,15 @@
-import { Application } from "express";
+import { Router } from "express";
 import authenticateToken from "../middlewares/authenticateToken";
+import getAllAlbumController from "@/controllers/album/getAllAlbumController";
+import {
+    validateCreateAlbum,
+    validateGetAlbumById,
+} from "../middlewares/validate";
+import createAlbumController from "@/controllers/album/createAlbumController";
+import deleteAlbumByIdController from "@/controllers/album/deleteAlbumByIdController";
+import upload from "@/utils/multer";
 
-export default function (app: Application) {
+export default function (app: Router) {
     app.get(
         '/album',
         authenticateToken(true, 'user', 'admin'),
@@ -21,7 +29,7 @@ export default function (app: Application) {
     app.delete(
         '/album/:album_id',
         authenticateToken(true, 'user', 'admin'),
-        validateDeleteAlbumById,
+        validateGetAlbumById,
         deleteAlbumByIdController
     )
 }
